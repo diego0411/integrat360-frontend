31b02253ea37aebbc9c7d58510f534744d60a26a
diego0411 <raydiegord@gmail.com>
2025-02-07 09:26:03 -0400

    conexion con el backenddesplegado 4

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function AppContent() {
             {location.pathname !== "/login" && <Navbar />}
 
             <Routes>
-                <Route path="/" element={<Dashboard />} />
+                <Route path="/" element={<Login />} />
                 <Route path="/managegroups" element={<ManageGroups />} />
                 <Route path="/manageevents" element={<ManageEvents />} />
                 <Route path="/login" element={<Login />} />
