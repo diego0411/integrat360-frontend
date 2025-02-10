2cada370c26e7666db9658e612e2b5aa5f12a396
diego0411 <raydiegord@gmail.com>
2025-02-10 14:31:23 -0400

    frontend ejorado 12

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import "/src/styles/Dashboard.css";
+import "../styles/Dashboard.css";
+
 
 
 function Dashboard() {
